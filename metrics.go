df95b5251e73551f738fa87eca36f45f97b3700c
Korjavin Ivan <korjavin@gmail.com>
2018-09-24 18:56:18 +0200

    Add SimpleMetric realization

--- metrics.go
+++ metrics.go
@@ -11,3 +11,31 @@ type Metric interface {
 	Values() map[string]interface{}
 	Time() time.Time
 }
+
+// SimpleMetric is a simple struct that doesn't produce any calculatons just return it as-is
+type SimpleMetric struct {
+	Name       string
+	TagsMap    map[string]string
+	ValuesMap  map[string]interface{}
+	CreateTime time.Time
+}
+
+// Measurement returns Measurement name
+func (r SimpleMetric) Measurement() string {
+	return r.Name
+}
+
+// Tags returns  prepared tags
+func (r SimpleMetric) Tags() map[string]string {
+	return r.TagsMap
+}
+
+// Values returns prepared values
+func (r SimpleMetric) Values() map[string]interface{} {
+	return r.ValuesMap
+}
+
+//Time return time for the value
+func (r SimpleMetric) Time() time.Time {
+	return r.CreateTime
+}
